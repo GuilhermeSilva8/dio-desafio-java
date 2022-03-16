b8773d96ed0bed16365646e9353164503ebb1928
Guilherme <guilhermecsilva8@gmail.com>
2022-03-16 11:58:33 -0300

    Classes e teste feitos na aula

--- DIO_Desafio_Java/src/App.java
+++ DIO_Desafio_Java/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Cliente guilherme = new Cliente();
+        guilherme.setNome("guilherme");
+        
+        Conta cc = new ContaCorrente(guilherme);
+        Conta poupanca = new ContaPoupanca(guilherme);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
     }
 }
